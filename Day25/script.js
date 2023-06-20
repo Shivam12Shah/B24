6999f70d9eb4350b6d7cc088ebaf4c854b25ad35
unknown <shivamshah1210@gmail.com>
2023-06-20 11:51:02 +0530

    day 35

--- Day25/script.js
+++ Day25/script.js
@@ -12,3 +12,25 @@
 // textcontent jab bas element ke ander ka change karna hai
 //
 // 
+var btn = document.querySelector("button");
+var h4 = document.querySelector("#card h4");
+
+var flag = 0;
+
+btn.addEventListener("click", function () {
+    if (flag === 0) {
+        h4.textContent = "Friend";
+        h4.style.color = "green";
+        btn.textContent = "Remove Friend";
+        flag = 1
+
+        h4.style.transition = "all ease 3s";
+    } else {
+        h4.textContent = "Stranger";
+        h4.style.color = "red";
+        btn.textContent = "Add Friend";
+        flag = 0;
+        btn
+        .style.transition = "all ease 3s";
+    }
+})
