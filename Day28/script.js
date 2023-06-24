876ac87c9b81648338be03cc277b7ec4b31af1fc
unknown <shivamshah1210@gmail.com>
2023-06-24 13:21:47 +0530

    Pura ban dia

--- Day28/script.js
+++ Day28/script.js
@@ -13,5 +13,6 @@ img1.addEventListener("mousemove", function(){
 })
 img1.addEventListener("mouseout", function(){
     main.style.backgroundColor = "gray"
-    
+    h1.innerHTML = "lo bhai lawde bhi lag gaye"
+    img2.setAttribute("src", "https://images.unsplash.com/photo-1533729590644-695ded775a63?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fGtvcmVhbiUyMGdpcmx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60")
 })
