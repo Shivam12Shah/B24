da8d6de1a369a372b951da87545fc08d7821ed25
unknown <shivamshah1210@gmail.com>
2023-06-24 13:16:31 +0530

    Day 28

---
+++ Day28/script.js
@@ -0,0 +1,17 @@
+
+
+var main = document.querySelector("#main");
+var img1 = document.querySelector("#img1");
+var img2 = document.querySelector("#img2");
+var h1 = document.querySelector("h1");
+
+
+img1.addEventListener("mousemove", function(){
+    main.style.backgroundColor = "red"
+    img2.setAttribute("src", "https://images.unsplash.com/photo-1593412544031-ffedb2876ed6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80")
+    h1.innerHTML = "Lo bhai kaam ho gaya"
+})
+img1.addEventListener("mouseout", function(){
+    main.style.backgroundColor = "gray"
+    
+})
