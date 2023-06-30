cbbb2e4914f61115a72e975050d316eabc455854
unknown <shivamshah1210@gmail.com>
2023-06-30 22:41:30 +0530

    Day 31

---
+++ Day31/script.js
@@ -0,0 +1,17 @@
+var h_1 = document.querySelectorAll("h1");
+
+
+
+
+
+h_1.forEach(function(dets){
+    dets.addEventListener("mouseenter", function(){
+        dets.style.color = "white";
+        dets.style.backgroundColor = "black"
+    })
+    dets.addEventListener("mouseleave", function(){
+        dets.style.color = "black";
+        dets.style.backgroundColor = "white"
+    })
+    
+})
