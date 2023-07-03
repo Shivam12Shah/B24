cee907587876ddcc8cbd7f0c1fa651a2540e1499
unknown <shivamshah1210@gmail.com>
2023-07-03 23:04:36 +0530

    Day 32

--- 2practice/script.js
+++ 2practice/script.js
@@ -5,11 +5,39 @@ var slid = document.querySelectorAll(".slid")
 // console.log(slid);
 
 // console.log(slider);
+var value = 0
 
-btn1.addEventListener("click", function(){
-    slid.forEach(function(dets){
-        slider.style.backgroundColor = "green";
-        slider.style.left = "-450px"
-    })
+
+btn2.addEventListener("click", function () {
+    if(value<0){
+        value += 100
+        slid.forEach(function (dets) {
+            // console.log(dets);
+            dets.style.transform = `translate(${value}%, 0)`;
+            console.log(value);
+    
+    
+        })
+    }
+    else{
+        console.log("aa gaye mder");
+    }
 
 })
+
+btn1.addEventListener("click", function () {
+    if(value > -500){
+        value -= 100
+        slid.forEach(function (dets) {
+
+            // console.log(dets);
+            dets.style.transform = `translate(${value}%, 0)`;
+            console.log(value);
+    
+        })
+    }
+    else{
+        console.log("bhag ja mac");
+    }
+})
+
---
+++ Day32/script.js
@@ -0,0 +1,14 @@
+// setInterval(function(){
+//     console.log("interval");
+// }, 1000)
+
+// setTimeout(function(){
+//     console.log("settimout");
+// },5874398)
+
+
+// IIFE intermediate ivocked function expression
+
+(function abc(){
+    console.log("mahesh dallle");
+})()
