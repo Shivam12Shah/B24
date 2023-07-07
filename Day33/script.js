fef91b6195321d49a2c00b5bb73c094d3264deb9
unknown <shivamshah1210@gmail.com>
2023-07-07 21:31:50 +0530

    game alomost compleste

--- Day33/script.js
+++ Day33/script.js
@@ -2,15 +2,38 @@
 var botm = document.querySelector("#botm");
 
 var top = document.querySelector("#top")
-for(var i=0; i<89; i++){
-    var rn= Math.floor(Math.random()*10);
-    botm.innerHTML += `<div class="bubble">${rn}</div>`
+var timer = 60;
+var scrore = 0;
+function bubbleCreate(){
+    botm.innerHTML = "";
+    for(var i=0; i<90; i++){
+        var rn= Math.floor(Math.random()*10);
+        botm.innerHTML += `<div class="bubble">${rn}</div>`
+    }
+    
 }
 
-botm.addEventListener("click", function(dets){
-    console.log(dets.target);
-})
-var scr =60;
-setTimeout(() => {
-    console.log(60 - 1 );
-},500);
+function startTimer(){
+    setInterval(function(){
+        timer--;
+        if(timer>=0){
+            document.querySelector("#timmer-div").textContent = `${timer}`;
+        }
+    },1000)
+}
+
+
+function getHit(){
+    document.querySelector("#hit").textContent = `${Math.floor(Math.random()*10)}`
+}
+
+
+function score(){
+    scrore+=10;
+    document.querySelector("#score").textContent = `${scrore}`
+}
+
+
+bubbleCreate();
+getHit();
+startTimer();
